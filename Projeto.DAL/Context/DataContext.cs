57c25a4464fd5f42481dce29bfd1f10c26215e55
Lucarv90 <luizaylton@gmail.com>
2018-10-07 11:54:22 -0300

    subindo projeto

---
+++ Projeto.DAL/Context/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.DAL.Mappings;
+using Projeto.Entidades;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Projeto.DAL.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base(ConfigurationManager.ConnectionStrings["estudo"].ConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AlunoMap());
+        }
+        public DbSet<Aluno> Aluno { get; set; }
+    }
+}
---
+++ Projeto.Entidades/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Aluno
+    {
+        public int IdAluno { get; set; }
+        public string Matricula { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(int idAluno, string matricula, string nome, DateTime dataNascimento)
+        {
+            IdAluno = idAluno;
+            Matricula = matricula;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+        }
+
+        public override string ToString()
+        {
+            return $"Id Aluno: {IdAluno}, Matricula: {Matricula}, Nome: {Nome}, Data de Nascimento: {DataNascimento}";
+        }
+    }
+}
